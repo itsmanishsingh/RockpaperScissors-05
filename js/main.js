81f60eb50dd929c121f5b76cd253b50dc7a2bee3
itsmanishsingh <itsmanishsingh6@gmail.com>
2023-03-07 23:21:12 +0530

    Project initate

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+let userscore = document.getElementById("userScoreVal").value;
+let computerscore = document.getElementById("compScoreVal").value;
+
+
+let userresult = document.querySelectorAll(".choices");
+
